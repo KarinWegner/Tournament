38320c7d95374b114561232153db8a295cdf5d87
Karin Wegner <karinwegner00@gmail.com>
2024-11-28 09:45:32 +0100

    Moved Data folder to Tournament.Data

--- Tournament.Core/Entities/Game.cs
+++ Tournament.Core/Entities/Game.cs
@@ -12,6 +12,6 @@ namespace Tournament.Core.Entities
         public string Title { get; set; }
         public DateTime Time { get; set; }
 
-        int TournamentId { get; set; }
+        public int TournamentId { get; set; }
     }
 }
