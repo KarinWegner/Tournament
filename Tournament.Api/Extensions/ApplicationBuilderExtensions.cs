cbef686bdcf47674d8277e2148be8da83f2f781a
Karin Wegner <karinwegner00@gmail.com>
2024-11-28 11:35:31 +0100

    Moved Seeder into SeedData

--- Tournament.Api/Extensions/ApplicationBuilderExtensions.cs
+++ Tournament.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -15,14 +15,9 @@ namespace Tournament.Api.Extensions
                 var serviceprovider = scope.ServiceProvider;
                 var db = serviceprovider.GetRequiredService<TournamentApiContext>();
 
-                await db.Database.MigrateAsync();
-                if (await db.TournamentDetails.AnyAsync()) return;
-
                 try
                 {
-                    var tournaments = GenerateTournaments(5);
-                    db.AddRange(tournaments);
-                    await db.SaveChangesAsync();
+                    await SeedData.InitAsync(db);
                 }
                 catch (Exception ex)
                 {
@@ -33,45 +28,6 @@ namespace Tournament.Api.Extensions
 
         }
 
-        private static IEnumerable<TournamentDetails> GenerateTournaments(int numberOfTournaments)
-        {
-            string[] sports = ["Football", "Hockey", "Rugby", "Basketball", "Baseball", "Tennis"];
-            string[] tournamentStages = ["Qualifier", "Quarterfinal", "Semifinal", "Finals"];
 
-
-            var faker = new Faker<TournamentDetails>("sv").Rules((f, t) =>
-            {
-                t.Title = TitleBuilder(
-                    f.Address.City(),
-                    sports[f.Random.Int(0, sports.Length - 1)],
-                    tournamentStages[f.Random.Int(0, tournamentStages.Length - 1)]
-                    );
-                t.StartDate = f.Date.Soon();
-                t.Games = GenerateGames(f.Random.Int(min: 3, max: 8), t.StartDate);
-            });
-
-            return faker.Generate(numberOfTournaments);
-        }
-
-        private static ICollection<Game> GenerateGames(int numberOfGames, DateTime startDate)
-        {
-            //ToDo: make more fun
-            
-            int matchCounter = 1;
-            TimeSpan matchTime = new (03, 30, 0);
-            var faker = new Faker<Game>("sv").Rules((f, g) =>
-            {
-                g.Title = "Match " + matchCounter++;
-                g.Time = f.Date.Between(startDate, startDate.Add(matchTime));
-
-            });
-
-            return faker.Generate(numberOfGames);
-        }
-
-        private static string TitleBuilder(string location, string sport, string stage)
-        {
-            return $"{location} {sport} {stage}";
-        }
     }
 }
---
+++ Tournament.Data/Data/SeedData.cs
@@ -0,0 +1,72 @@
+﻿using Bogus;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tournament.Core.Entities;
+
+namespace Tournament.Data.Data
+{
+    public class SeedData
+    {
+        public static async Task InitAsync(TournamentApiContext context)
+        {
+            await context.Database.MigrateAsync();
+            if (await context.TournamentDetails.AnyAsync()) return;
+
+            try
+            {
+                var tournaments = GenerateTournaments(5);
+                context.AddRange(tournaments);
+                await context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+        private static IEnumerable<TournamentDetails> GenerateTournaments(int numberOfTournaments)
+        {
+            string[] sports = ["Football", "Hockey", "Rugby", "Basketball", "Baseball", "Tennis"];
+            string[] tournamentStages = ["Qualifier", "Quarterfinal", "Semifinal", "Finals"];
+
+
+            var faker = new Faker<TournamentDetails>("sv").Rules((f, t) =>
+            {
+                t.Title = TitleBuilder(
+                    f.Address.City(),
+                    sports[f.Random.Int(0, sports.Length - 1)],
+                    tournamentStages[f.Random.Int(0, tournamentStages.Length - 1)]
+                    );
+                t.StartDate = f.Date.Soon();
+                t.Games = GenerateGames(f.Random.Int(min: 3, max: 8), t.StartDate);
+            });
+
+            return faker.Generate(numberOfTournaments);
+        }
+
+        private static ICollection<Game> GenerateGames(int numberOfGames, DateTime startDate)
+        {
+            //ToDo: make more fun
+
+            int matchCounter = 1;
+            TimeSpan matchTime = new(03, 30, 0);
+            var faker = new Faker<Game>("sv").Rules((f, g) =>
+            {
+                g.Title = "Match " + matchCounter++;
+                g.Time = f.Date.Between(startDate, startDate.Add(matchTime));
+
+            });
+
+            return faker.Generate(numberOfGames);
+        }
+
+        private static string TitleBuilder(string location, string sport, string stage)
+        {
+            return $"{location} {sport} {stage}";
+        }
+    }
+}
