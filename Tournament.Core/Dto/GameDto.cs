43fb3aff73c4236f008987fbccbbaa09156c6aba
Karin Wegner <karinwegner00@gmail.com>
2024-12-02 16:57:00 +0100

    Began work on Dto's

---
+++ Tournament.Core/Dto/GameDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tournament.Core.Dto
+{
+    internal class GameDto
+    {
+        public string Title { get; set; }
+        public DateTime StartTime { get; set; }
+    }
+}
